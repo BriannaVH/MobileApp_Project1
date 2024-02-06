f5cb53f88038cd7b12b75e2643092405500f0da7
Brianna Van Hecke <vanheckeb@merrimack.edu>
2024-02-06 12:58:14 -0500

    fixed clr button, changed alignment of GPA calc

--- app/src/main/java/com/example/bottombardemo/screens/Academics.kt
+++ app/src/main/java/com/example/bottombardemo/screens/Academics.kt
@@ -5,19 +5,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -30,6 +27,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.bottombardemo.Course
@@ -134,6 +132,7 @@ fun Academics(
                 searching = false
                 courseName = ""
                 courseCreditHour = ""
+                letterGrade = ""
             }// ,
 //                modifier = Modifier.size(width = 80.dp, height = 50.dp)
 
@@ -153,9 +152,11 @@ fun Academics(
             }
         }
 
-        Row {
-            Spacer(modifier = Modifier.weight(1f))
-            Text("GPA: " + calculatedGPA.toString())
+        Row () {
+            Surface(color = Color.Green) {
+                Spacer(modifier = Modifier.weight(1f))
+                Text("GPA: " + calculatedGPA.toString(), textAlign = TextAlign.Left)
+        }
 
         }
         LazyColumn(
@@ -166,7 +167,7 @@ fun Academics(
             val list = if (searching) searchResults else allCourses
 
             item {
-                TitleRow(head1 = "ID", head2 = "Course", head3 = "CreditHour", head4 = "LetterGrade")
+                TitleRow(head1 = "ID", head2 = "Course", head3 = "Credit Hours", head4 = "Grades")
             }
 
             items(list) { course ->

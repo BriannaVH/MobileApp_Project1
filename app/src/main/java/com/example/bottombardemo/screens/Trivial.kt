6dcda4c70e98243ce257ccd8dba11dd24c566b60
grayd173 <grayd@merrimack.edu>
2024-02-20 16:54:37 -0500

    basic question object is now working

--- app/src/main/java/com/example/bottombardemo/screens/Trivial.kt
+++ app/src/main/java/com/example/bottombardemo/screens/Trivial.kt
@@ -1,37 +1,34 @@
 package com.example.bottombardemo.screens
 
-import android.widget.EditText
-import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.text.isDigitsOnly
 
 @Composable
 @Preview
@@ -52,9 +49,13 @@ fun Trivial() {
                 /*TODO*/
             }, modifier = Modifier.align(CenterHorizontally)
 
-            ){
-                Text(text = "Start")
-            }
+        ) {
+            Text(text = "Start")
+        }
+        
+        Question(QuestionStr = "Example question 1")
+
+
     }
 }
 
@@ -65,21 +66,21 @@ fun Trivial() {
  * https://stackoverflow.com/questions/13508346/regular-expression-for-integer-greater-than-0-and-less-than-11
  */
 @Composable
-fun numberInputField(){
-    var input by remember { mutableStateOf("")}
-    var regex = remember {Regex("^[1-9]\$|^10\$")}
+fun numberInputField() {
+    var input by remember { mutableStateOf("") }
+    var regex = remember { Regex("^[1-9]\$|^10\$") }
 
-    val onChange = { text : String ->
-        if(text.isEmpty() || text.matches(regex)){
+    val onChange = { text: String ->
+        if (text.isEmpty() || text.matches(regex)) {
             input = text
         }
     }
 
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlignment = CenterHorizontally,
         modifier = Modifier
             .fillMaxWidth()
-    ){
+    ) {
         CustomTextField(
             title = "Number of Questions",
             textState = input,
@@ -92,8 +93,58 @@ fun numberInputField(){
 
 /**
  * Template that constructs a question gui component
+ *
+ * Getting context
+ * https://stackoverflow.com/questions/58743541/how-to-get-context-in-jetpack-compose
  */
 @Composable
-fun Question(){
+fun Question(QuestionStr : String) {
 
+
+    //List of options. This represents the answers that will be given by the user
+    val options = listOf("A", "B", "C", "D")
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(options[2]) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = CenterHorizontally
+    ) {
+        Column {
+            Text(text = QuestionStr,
+                modifier = Modifier.align(CenterHorizontally)) //The text entry for the question
+
+            //Add a radio button for each option
+            options.forEach { text ->
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .selectable(
+                            selected = (text == selectedOption),
+                            onClick = { onOptionSelected(text) }
+                        )
+
+                        .padding(horizontal = 16.dp)
+                ) {
+                    val context = LocalContext.current
+                    RadioButton(selected = (text == selectedOption),
+                        modifier = Modifier.padding(8f.dp),
+                        onClick = {
+                            onOptionSelected(text)
+
+                            Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+                        }
+                    )
+
+                    //The answer that goes along with this button
+                    Text(
+                        text = text,
+                        modifier = Modifier.align(CenterVertically)
+                    )
+                }
+            }
+        }
+    }
 }
